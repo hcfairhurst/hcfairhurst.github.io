8ac34e653af5f0c765a396117773081d4379511d
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-06-16 14:58:48 -0600

    added functional json fetch

--- lesson9/wk-9.js/home.js
+++ lesson9/wk-9.js/home.js
@@ -28,5 +28,44 @@ function toggleMenu() {
       pancake.style.display = "block";
     }
   }
-  
   showPancake()
+
+  /*this is the json fetch*/
+  const  requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+  fetch(requestURL)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const towns = jsonObject['towns'];
+
+    
+for (let i = 0; i <= 2; i++){
+  let info = document.createElement('div');
+  let h2 = document.createElement('h2');
+  let motto = document.createElement('h4');
+  let year = document.createElement('p');
+  let pop = document.createElement('p');
+  let rain = document.createElement('p');
+ 
+
+h2.textContent = towns[i].name;
+motto.textContent = towns[i].motto;
+year.textContent = 'Year Founded: ' + towns[i].yearFounded;
+pop.textContent = 'Current Population: ' + towns[i].currentPopulation;
+rain.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+
+
+
+info.appendChild(h2);
+info.appendChild(motto);
+info.appendChild(year);
+info.appendChild(pop);
+info.appendChild(rain);
+
+document.querySelector('section.data').appendChild(info);
+}
+
+
+  })
