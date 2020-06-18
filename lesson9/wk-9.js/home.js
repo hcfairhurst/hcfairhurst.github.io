c0efc544a4eb45855ff3e4d819ac8710d36929cd
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-06-18 12:46:18 -0600

    css for small and medium

--- lesson9/wk-9.js/home.js
+++ lesson9/wk-9.js/home.js
@@ -1,71 +1,86 @@
 /* this is the navigation drop menu in the small view */
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-  }
-  /* this is the current date in the footer*/
-  try {
-    const options = {
-      weekday: 'long',
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    };
-    document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-  }
-  /* this changes the displayed number on the slider*/
-  function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-  /*this is the pancake banner*/
-  function showPancake() {
-    var date = new Date();
-    var dayOfWeek = date.getDay();
-    
-    if (dayOfWeek == 5) {
-      var pancake = document.getElementById("pancake");
-      pancake.style.display = "block";
-    }
-  }
-  showPancake()
+/* this is the current date in the footer*/
+try {
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+  document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options);
+} catch (e) {
+  alert('Error with code or your browser does not support Locale');
+}
+/* this changes the displayed number on the slider*/
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+/*this is the pancake banner*/
+function showPancake() {
+  var date = new Date();
+  var dayOfWeek = date.getDay();
 
-  /*this is the json fetch*/
-  const  requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-  fetch(requestURL)
-  .then(function (response){
+  if (dayOfWeek == 5) {
+    var pancake = document.getElementById("pancake");
+    pancake.style.display = "block";
+  }
+}
+showPancake()
+
+/*this is the json fetch*/
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+fetch(requestURL)
+  .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
     const towns = jsonObject['towns'];
 
-    
-for (let i = 0; i <= 2; i++){
-  let info = document.createElement('div');
-  let h2 = document.createElement('h2');
-  let motto = document.createElement('h4');
-  let year = document.createElement('p');
-  let pop = document.createElement('p');
-  let rain = document.createElement('p');
- 
+    for (let i = 0; i < towns.length; i++) {
+      let town = towns[i]
+      let townNames = ["Fish Haven", "Preston", "Soda Springs"]
+      if (townNames.includes(town.name)) {
 
-h2.textContent = towns[i].name;
-motto.textContent = towns[i].motto;
-year.textContent = 'Year Founded: ' + towns[i].yearFounded;
-pop.textContent = 'Current Population: ' + towns[i].currentPopulation;
-rain.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+        let section = document.createElement('section');
+        section.classList.add('town');
+        // info div
+        let info = document.createElement('div');
+        info.classList.add('info');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('h4');
+        let year = document.createElement('p');
+        let pop = document.createElement('p');
+        let rain = document.createElement('p');
+        h2.textContent = town.name;
+        motto.textContent =  'Motto: ' + town.motto;
+        year.textContent = 'Year Founded: ' + town.yearFounded;
+        pop.textContent = 'Current Population: ' + town.currentPopulation;
+        rain.textContent = 'Average Rainfall: ' + town.averageRainfall;
+        info.appendChild(h2);
+        info.appendChild(motto);
+        info.appendChild(year);
+        info.appendChild(pop);
+        info.appendChild(rain);
+        // picture div
+        let picture = document.createElement('div')
+        picture.classList.add('picture');
+        let img = document.createElement('img');
+        img.setAttribute('src', "images9/" + town.photo);
+        picture.appendChild(img);
+        // append info and picture to section
+        section.appendChild(picture)
+        section.appendChild(info)
+        
+
+       document.getElementById('towns').appendChild(section);
+      }
 
 
-
-info.appendChild(h2);
-info.appendChild(motto);
-info.appendChild(year);
-info.appendChild(pop);
-info.appendChild(rain);
-
-document.querySelector('section.data').appendChild(info);
-}
+    }
 
 
   })
