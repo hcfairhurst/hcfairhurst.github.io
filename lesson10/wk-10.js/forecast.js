368b288cc532ed8124cd7e857a4e038c4a7bae7d
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-06-23 12:16:56 -0600

    added js. and html id's for json weather Sum info

--- lesson10/wk-10.js/forecast.js
+++ lesson10/wk-10.js/forecast.js
@@ -3,12 +3,11 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {/*this is the variable for the json data we requested*/
     console.log(jsObject);/* this check to see if the json data is being called*/
-    document.getElementById('current').textContent = jsObject.weather.description;
+    document.getElementById('current').textContent = "Currently : " + jsObject.weather.main;
+    document.getElementById('temp').textContent = "High : " + jsObject.main.temp;
+    document.getElementById('humid').textContent = "Humidity : " + jsObject.main.humidity + "%";
+    document.getElementById('wind').textContent = jsObject.wind.speed;
     /* we created a span with the id of current-temp, we are assigning the json data 
      main.temp to that Id and placing it in the HTML span*/
-     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-     const desc = jsObject.weather[0].description;  // note how we reference the weather array
-     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-     document.getElementById('icon').setAttribute('alt', desc);
+     
   });
