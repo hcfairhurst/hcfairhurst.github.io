f19dea6c73ae4779c8a6d278a6dc7985353eda74
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-07-06 15:20:06 -0600

    set up of website plan

---
+++ lesson12/main.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+ }
+
+ try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
