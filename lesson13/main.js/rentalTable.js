eda4cc3ca7b6ccc090cb4acbb924b0e8de384ad8
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-07-13 12:01:07 -0600

    rental json file, trouble shoot table small mq

---
+++ lesson13/main.js/rentalTable.js
@@ -0,0 +1,18 @@
+const requestURL = "https://byui-cit230.github.io/lesson13/data/rentalinfo.json";
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+            const towns = jsonObject['towns'];
+            const townName = towns.filter(x => x.name.includes("Soda Springs"));
+            console.log(townName)
+            const events = townName[0].events;
+
+            events.forEach(event => {
+                let party = document.createElement('p');
+                party.innerText = event
+                let eventInfo = document.getElementById('event-info');
+                eventInfo.appendChild(party)
+            })
+        })
