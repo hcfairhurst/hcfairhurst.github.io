949af2e722a52957ce5d830019406f65a55d2a7f
Heidi Fairhurst <hcfairhurst@gmail.com>
2020-07-14 12:14:20 -0600

    fixed json objects try to fix rental js. form css

--- lesson13/main.js/rentalTable.js
+++ lesson13/main.js/rentalTable.js
@@ -5,13 +5,36 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
             const rentals = jsonObject['rentals'];
-            console.log(rentals)
-            const atv = rentals[0].name;
+            /*const scooter = rentals.filter(x => x.name.includes("Honda Metro Scooter"));*/
+           
 
-            atv.forEach(vehicle => {
-                let info = document.createElement('td');
-                info.innerText = vehicle
-                let vehicleInfo = document.getElementById('Metro');
-                vehicleInfo.appendChild(info)
-            })
+            for (let i = 0 ; i < rentals.length; i++){
+            
+            let row = document.createElement('tr');
+              let name = document.createElement('td');
+              let column = document.createElement('td');
+              let maxpep = document.createElement('td');
+              let reserveHalf = document.createElement('td');
+              let reserveFull = document.createElement('td');
+              let walkHalf = document.createElement('td');
+              let walkFull = document.createElement('td');
+
+              name.textContent = rentals[i].name;
+              maxpep.textContent = rentals[i].maxpersons;
+              reserveHalf.textContent = rentals[i].reservationhalfday;
+              reserveFull.textContent = rentals[i].reservationfullday;
+              walkHalf.textContent = rentals[i].walkinhalfday;
+              walkFull.textContent = rentals[i].walkinfullday;
+
+                row.appendChild(name);
+                row.appendChild(maxpep);
+                row.appendChild(reserveHalf);
+                row.appendChild(walkHalf);
+                row.appendChild(walkFull);
+
+              document.getElementById('row').appendChild(row);
+              
+              
+            }
+           
         })
